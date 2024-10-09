677997d1cd35a455ca303d4885df5146b2255d8f
Sarah Culpepper <sarah.culpepper2022@gmail.com>
2024-10-09 11:36:23 -0500

    added all html pages, views and urls, made navbar.html and included it in the other templates

--- storytrackpro/storytrackpro/urls.py
+++ storytrackpro/storytrackpro/urls.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
 from django.urls import path
-from .views import homepage_view
+from .views import *
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', homepage_view, name='homepage'),
+    path('', homepage_view, name='home'),
+    path('home/', homepage_view, name='home'),
+    path('dashboard/', dashboard_view, name='dashboard'),
+    path('assignments/', assignments_view, name='assignments'),
+    path('story-status/', status_view, name='status'),
+    path('submit-story/', submit_view, name='submit'),
 ]
 
 if settings.DEBUG:
--- storytrackpro/storytrackpro/views.py
+++ storytrackpro/storytrackpro/views.py
@@ -3,3 +3,19 @@ from django.shortcuts import render
 def homepage_view(request):
     template_name = 'homepage.html'
     return render(request, template_name)
+
+def dashboard_view(request):
+    template_name = 'dashboard.html'
+    return render(request, template_name)
+
+def assignments_view(request):
+    template_name = 'assignments.html'
+    return render(request, template_name)
+
+def status_view(request):
+    template_name = 'status.html'
+    return render(request, template_name)
+
+def submit_view(request):
+    template_name = 'submit.html'
+    return render(request, template_name)
