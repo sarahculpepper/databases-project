e610d2e81ff9de03e927e0169b0c35b4dad1fe32
Sarah Culpepper <sarah.culpepper2022@gmail.com>
2024-12-18 12:32:11 -0600

    restructured code

--- storytrackpro/views.py
+++ storytrackpro/views.py
@@ -1,13 +1,15 @@
-from django.shortcuts import redirect, render
-from .sql_queries import *
-from .create_reporter_editor import *
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect
-from django.urls import reverse
-from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login
-from django.contrib import messages
 import time
+
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.shortcuts import redirect, render
+from django.urls import reverse
+
+from .create_reporter_editor import *
+from .sql_queries import *
+
 
 def register_view(request):
     if request.method == "POST":
